b4038650f162d86cf8d8caf5b4091443b99f91f9
Betty <827941041@qq.com>
2024-08-13 16:05:12 -0400

    Simple FTP client with upload and download + async

--- FTPClient/Assets/Scripts/FTPComponent.cs
+++ FTPClient/Assets/Scripts/FTPComponent.cs
@@ -7,35 +7,80 @@ public class FTPComponent : MonoBehaviour
 {
     void Start()
     {
-        try
-        {
-            FtpWebRequest req = FtpWebRequest.Create(new Uri("ftp://127.0.0.1/pic.png")) as FtpWebRequest;
-            req.Proxy = null;
-            NetworkCredential n = new NetworkCredential("xxx", "xxx");
-            req.Credentials = n;
-            req.KeepAlive = false;
-            req.Method = WebRequestMethods.Ftp.UploadFile;
-            req.UseBinary = true;
-            
-            Stream upLoadStream = req.GetRequestStream();
-            using (FileStream file = File.OpenRead(Application.streamingAssetsPath + "/Test.png"))
-            {
-                byte[] bytes = new byte[1024];
-                int contentLength = file.Read(bytes, 0, bytes.Length);
-                while (contentLength != 0)
-                {
-                    upLoadStream.Write(bytes,0,contentLength);
-                    contentLength = file.Read(bytes, 0, bytes.Length);
-                }
-                file.Close();
-                upLoadStream.Close();
-                print("FINISH UPLOAD");
-            }
-        }
-        catch (Exception e)
-        {
-            print("UPLOAD FAILED " + e.Message);
-        }
+        #region Simple upload
+        // try
+        // {
+        //     FtpWebRequest req = FtpWebRequest.Create(new Uri("ftp://127.0.0.1/pic.png")) as FtpWebRequest;
+        //     req.Proxy = null;
+        //     // TODO change to real username and password
+        //     NetworkCredential n = new NetworkCredential("xxx", "xxx");
+        //     req.Credentials = n;
+        //     req.KeepAlive = false;
+        //     req.Method = WebRequestMethods.Ftp.UploadFile;
+        //     req.UseBinary = true;
+        //     
+        //     Stream upLoadStream = req.GetRequestStream();
+        //     using (FileStream file = File.OpenRead(Application.streamingAssetsPath + "/Test.png"))
+        //     {
+        //         byte[] bytes = new byte[1024];
+        //         int contentLength = file.Read(bytes, 0, bytes.Length);
+        //         while (contentLength != 0)
+        //         {
+        //             upLoadStream.Write(bytes,0,contentLength);
+        //             contentLength = file.Read(bytes, 0, bytes.Length);
+        //         }
+        //         file.Close();
+        //         upLoadStream.Close();
+        //         print("FINISH UPLOAD");
+        //     }
+        // }
+        // catch (Exception e)
+        // {
+        //     print("UPLOAD FAILED " + e.Message);
+        // }
+        #endregion
+        #region Simple download 
+        // try
+        // {
+        //     FtpWebRequest req = FtpWebRequest.Create(new Uri("Ftp://127.0.0.1/pic.png")) as FtpWebRequest;
+        //     // TODO change to real username and password
+        //     req.Credentials = new NetworkCredential("xxx", "xxx");
+        //     req.KeepAlive = false;
+        //     req.Method = WebRequestMethods.Ftp.DownloadFile;
+        //     req.UseBinary = true;
+        //     req.Proxy = null;
+        //
+        //     FtpWebResponse res = req.GetResponse() as FtpWebResponse;
+        //     Stream downloadStream = res.GetResponseStream();
+        //
+        //     print(Application.persistentDataPath);
+        //     using (FileStream fileStream = File.Create(Application.persistentDataPath + "/DownloadPic.png"))
+        //     {
+        //         byte[] bytes = new byte[1024];
+        //         int contentLength = downloadStream.Read(bytes, 0, bytes.Length);
+        //         while (contentLength != 0)
+        //         {
+        //             fileStream.Write(bytes,0,contentLength);
+        //             contentLength = downloadStream.Read(bytes, 0, bytes.Length);
+        //         }
+        //         downloadStream.Close();
+        //         fileStream.Close();
+        //     }
+        //     print("DOWNLOAD SUCCESS");
+        // }
+        // catch (Exception e)
+        // {
+        //     print("DOWNLOAD FAILED " + e.Message);
+        //     throw;
+        // }
+        #endregion
         
+        FTPMgr.Instance.UploadFile("NewPic.png",Application.streamingAssetsPath + "/Test.png", () => {
+                print("Finish uploading");
+        });
+        
+        FTPMgr.Instance.DownloadFile("pic.png",Application.persistentDataPath + "/DownloadPic.png", () => {
+            print("Finish downloading");
+        });
     }
 }
--- FTPClient/Assets/Scripts/FTPMgr.cs
+++ FTPClient/Assets/Scripts/FTPMgr.cs
@@ -1,14 +1,90 @@
-
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
 
 public class FTPMgr
 {
     private static FTPMgr instance = new FTPMgr();
     public static FTPMgr Instance => instance;
 
-    private string FTP_PATH = "ftp://127.0.0.1";
+    private string FTP_PATH = "ftp://127.0.0.1/";
+    // TODO change to real username and password
+    private string USER_NAME = "xxx";
+    private string PASSWORD = "xxx";
 
-    public async void UpLoadFile(string fileName, string localPath)
+    public async void DownloadFile(string fileName, string remotePath, UnityAction action = null)
     {
-        
+        await Task.Run(() => {
+            try
+            {
+                FtpWebRequest req = FtpWebRequest.Create(new Uri(FTP_PATH + fileName)) as FtpWebRequest;
+                req.Credentials = new NetworkCredential(USER_NAME, PASSWORD);
+                req.KeepAlive = false;
+                req.UseBinary = true;
+                req.Method = WebRequestMethods.Ftp.DownloadFile;
+                req.Proxy = null;
+
+                Stream downloadStream = req.GetResponse().GetResponseStream();
+                using (FileStream fileStream = File.Create(remotePath))
+                {
+                    byte[] bytes = new byte[1024];
+                    int contentLength = downloadStream.Read(bytes, 0, bytes.Length);
+                    while (contentLength != 0)
+                    {
+                        fileStream.Write(bytes,0,contentLength);
+                        contentLength = downloadStream.Read(bytes, 0, bytes.Length);
+                    }
+                    downloadStream.Close();
+                    fileStream.Close();
+                }
+                Debug.Log("DOWNLOAD SUCCESS");
+            }
+            catch (Exception e)
+            {
+                Debug.Log("DOWNLOAD FAILED " + e.Message);
+                throw;
+            }
+            action?.Invoke();
+        });
+    }
+
+    // Multi thread upload to avoid blocking main thread
+    public async void UploadFile(string fileName, string localPath, UnityAction action = null)
+    {
+        await Task.Run(() => {
+            try
+            {
+                FtpWebRequest req = FtpWebRequest.Create(new Uri(FTP_PATH + fileName)) as FtpWebRequest;
+                req.Credentials = new NetworkCredential(USER_NAME, PASSWORD);
+                req.KeepAlive = false;
+                req.UseBinary = true;
+                req.Method = WebRequestMethods.Ftp.UploadFile;
+                req.Proxy = null;
+
+                Stream uploadStream = req.GetRequestStream();
+                using (FileStream fileStream = File.OpenRead(localPath))
+                {
+                    byte[] bytes = new byte[1024];
+                    int contentLength = fileStream.Read(bytes, 0, bytes.Length);
+                    while (contentLength != 0)
+                    {
+                        uploadStream.Write(bytes, 0, contentLength);
+                        contentLength = fileStream.Read(bytes, 0, bytes.Length);
+                    }
+                    fileStream.Close();
+                    uploadStream.Close();
+                }
+                Debug.Log("UPLOAD SUCCESS");
+            }
+            catch (Exception e)
+            {
+                Debug.Log("UPLOAD FAILED " + e.Message);
+                throw;
+            }
+        });
+        action?.Invoke();
     }
 }
